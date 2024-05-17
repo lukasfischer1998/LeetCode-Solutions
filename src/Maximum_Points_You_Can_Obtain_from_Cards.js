c72a09653f407ab41c2e0c4f6af2f2e407a2f818
Ohboy166 <lukifisch@online.de>
2024-05-17 07:49:46 +0200

    1423. Maximum Points You Can Obtain from Cards

---
+++ src/Maximum_Points_You_Can_Obtain_from_Cards.js
@@ -0,0 +1,60 @@
+/**
+ * 1423. Maximum Points You Can Obtain from Cards
+ * Medium
+ *
+ * There are several cards arranged in a row, and each card has an associated number of points.
+ * The points are given in the integer array cardPoints.
+ *
+ * In one step, you can take one card from the beginning or from the end of the row. You have to take exactly k cards.
+ *
+ * Your score is the sum of the points of the cards you have taken.
+ *
+ * Given the integer array cardPoints and the integer k, return the maximum score you can obtain.
+ *
+ * Example 1:
+ * Input: cardPoints = [1,2,3,4,5,6,1], k = 3
+ * Output: 12
+ * Explanation: After the first step, your score will always be 1. However, choosing the rightmost card first
+ * will maximize your total score. The optimal strategy is to take the three cards on the right,
+ * giving a final score of 1 + 6 + 5 = 12.
+ *
+ * Example 2:
+ * Input: cardPoints = [2,2,2], k = 2
+ * Output: 4
+ * Explanation: Regardless of which two cards you take, your score will always be 4.
+ *
+ * Example 3:
+ * Input: cardPoints = [9,7,7,9,7,7,9], k = 7
+ * Output: 55
+ * Explanation: You have to take all the cards. Your score is the sum of points of all cards.
+ *
+ * Constraints:
+ * 1 <= cardPoints.length <= 105
+ * 1 <= cardPoints[i] <= 104
+ * 1 <= k <= cardPoints.length
+ *
+ * @param {number[]} cardPoints
+ * @param {number} k
+ * @return {number}
+ */
+//call it jumping back and forth
+var maxScore = function (cardPoints, k) {
+  let maxScore = 0;
+
+  //sum forst k cards
+  let currentSum = cardPoints.slice(0, k).reduce((sum, card) => sum + card, 0);
+  maxScore = currentSum;
+
+  //all possibles for sums
+  for (let i = 1; i <= k; i++) {
+    currentSum += cardPoints[cardPoints.length - i] - cardPoints[k - i];
+    maxScore = Math.max(maxScore, currentSum);
+  }
+
+  return maxScore;
+};
+
+// Test cases
+console.log(maxScore([1, 2, 3, 4, 5, 6, 1], 3)); // Expected output: 12
+console.log(maxScore([2, 2, 2], 2)); // Expected output: 4
+console.log(maxScore([9, 7, 7, 9, 7, 7, 9], 7)); // Expected output: 55
